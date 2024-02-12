a760f8ffa7e317a2ed382d95d54ad90dc908603c
SanaSanat <sst7212@gmail.com>
2024-02-12 00:24:02 -0800

    iframe

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -14,13 +14,14 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands"
+import "cypress-iframe";
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false
-})
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
