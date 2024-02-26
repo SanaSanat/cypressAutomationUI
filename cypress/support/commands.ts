acec84929507a2854b25d887452074bf1d9fa0bb
SanaSanat <sst7212@gmail.com>
2024-02-25 23:26:27 -0800

    readExcel

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -29,11 +29,29 @@ declare global {
   namespace Cypress {
     interface Chainable {
       login(email: string, password: string): Chainable<void>;
+      readingXlsx(file:any): any;
+      apiLogin(email: string, password: string): Chainable<void>;
     }
   }
 }
-Cypress.Commands.add("Login", (userName: string, password: string) => {
-  cy.get("#userName").type(userName);
-  cy.get("#password").type(password);
-  cy.get("#login").click();
+Cypress.Commands.add('Login', (userName: string, password: string) => {
+  cy.get('#userName').type(userName);
+  cy.get('#password').type(password);
+  cy.get('#login').click();
+});
+Cypress.Commands.add('readingXlsx', (file) => {
+  return cy.task('parseXlsx',{filePath:file});
+});
+Cypress.Commands.add('apiLogin', (email: string, password: string) => {
+   cy.request({
+    method: 'POST',
+    url: 'https://server-stage.pasv.us/user/login',
+    body: {
+      email: email,
+      password: password
+    }
+   }).then((response) => {
+    console.log(response);
+    
+   })
 });
